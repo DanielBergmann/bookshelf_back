b099a628bb259b7cdf0f9457a064ab0d76e8fab8
Daniel Lotsman <ddaannttee1155@gmail.com>
2020-07-21 21:04:03 +0300

    get or create author on book adding

--- shelf_back/db_manager.py
+++ shelf_back/db_manager.py
@@ -21,9 +21,9 @@ class BaseModel(Model):
         database = psql_db
 
 
-class Book(BaseModel):
+class Book2(BaseModel):
     title=CharField()
-    author=CharField()
+    authorid=IntegerField()
     read_status=IntegerField(default=0)
     priority=IntegerField(default=0)
     description=TextField(default='')
@@ -35,19 +35,52 @@ class Book(BaseModel):
         )
 
 
+class Author(BaseModel):
+    name = CharField()
+    description = CharField()
+    class Meta:
+        indexes = (
+            (('name','description'), True),
+        )
+
+
+def get_authors():
+    query = Author.select()
+    return query
+
+
+def get_author(author_id):
+    query = Author.select().where(Author.id == author_id)
+    return query
+
+
 def get_books():
-    query = Book.select().order_by(Book.id.asc())
+    query = Book2.select().order_by(Book2.id.asc())
     return query
 
 
 def get_book(book_id):
-    query = Book.select().where(Book.id == book_id)
+    query = Book2.select().where(Book2.id == book_id)
     return query
 
 
-def add_book(book_id, payload):
-    return 0
+def add_book(payload):
+    author = payload[1]
+    #test = list(get_authors_by_name(author).dicts())[0]['id']
+    test = get_authors_by_name(author)[0]
+    Book2.create(title=payload[0], author=test, read_status=0,description = payload[2], priority=payload[3])
+    return test
 
 
 def change_book(book_id, payload):
     return 0
+
+
+def get_authors_by_name(name):
+    query = Author.get_or_create(name = name)
+    return query
+
+#print(list(get_authors().dicts())[0])
+#print(list(get_authors_by_name().dicts()))
+#payload = ['some book', 'Bergmann', 'descr', '4']
+#print(add_book(payload))
--- webui.py
+++ webui.py
@@ -1,3 +1,5 @@
+#import utf8reader
+import json
 from bottle import (
     HTTPError,
     get,
@@ -14,6 +16,8 @@ from shelf_back.adapter import (
     get_book_details,
     add_book_to_storage,
     change_book_in_storage,
+    get_authors_all,
+    get_author_details,
 )
 @get('/')
 @get('/api/books')
@@ -21,9 +25,32 @@ def books():
     return {'response': get_books_all()}
 
 
-@get('/api/book/<book_id:int>')
+@get('/api/books/<book_id:int>')
 def book(book_id):
     return {'response': get_book_details(book_id)}
+
+
+@post('/api/books/add')
+def book_add():
+    title = request.forms.get('Title')
+    author = request.forms.get('Author')
+    description = request.forms.get('Description')
+    priority = request.forms.get('Priority')
+    request_payload = [title,author,description,priority]
+    #status = add_book_to_storage(request_payload)
+    status = 1
+    print(request_payload)
+    return {'response': request_payload} if status else {'response':'failed to add booking'}
+
+
+@get('/api/authors')
+def authors():
+    return {'response': get_authors_all()}
+
+
+@get('/api/authors/<author_id:int>')
+def book(author_id):
+    return {'response': get_author_details(author_id)}
 
 # @post('/follow/error/<error_id:int>')
 # def follow_error(error_id):
@@ -34,18 +61,18 @@ def book(book_id):
 #     } if notification_sent else HTTPError(400, 'Bad request')
 
 
-@post('api/book/<book_id:int>')
-def add_book(book_id):
-    request_payload = request.json
-    book_added = add_book_to_storage(book_id, request_payload['new_book'])
-    return {'response': book_added} if book_added else HTTPError(400, 'Bad request')
-
-
-@put('api/book/<book_id:int>')
-def change_book(book_id):
-    request_payload = request.json
-    book_changed = change_book_in_storage(book_id, request_payload['changed_book'])
-    return {'response': book_changed} if book_changed else HTTPError(400, 'Bad request')
+# @post('api/book/<book_id:int>')
+# def add_book(book_id):
+#     request_payload = request.json
+#     book_added = add_book_to_storage(book_id, request_payload['new_book'])
+#     return {'response': book_added} if book_added else HTTPError(400, 'Bad request')
+#
+#
+# @put('api/book/<book_id:int>')
+# def change_book(book_id):
+#     request_payload = request.json
+#     book_changed = change_book_in_storage(book_id, request_payload['changed_book'])
+#     return {'response': book_changed} if book_changed else HTTPError(400, 'Bad request')
 
 
 application = default_app()
